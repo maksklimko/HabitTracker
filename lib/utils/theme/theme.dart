0593fc1c7b40c9adc3325f2a3c324242c025c720
maksklimko <maks.klmqaz@gmail.com>
2024-11-13 19:06:54 +0200

    add font theme

--- lib/utils/theme/theme.dart
+++ lib/utils/theme/theme.dart
@@ -1,21 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:habit_tracker/utils/constants/colors.dart';
 
-class AppTheme{
+class AppTheme {
   AppTheme._();
 
-  //TODO:add font
-  //TODO:add text style
   static ThemeData lightTheme = ThemeData(
     useMaterial3: true,
-    brightness:  Brightness.light,
+    brightness: Brightness.light,
     primaryColor: AppColors.x08080a,
     scaffoldBackgroundColor: AppColors.xf6f6f7,
+    textTheme: TextTheme(
+      titleLarge: GoogleFonts.roboto(
+        fontSize: 38,
+        fontWeight: FontWeight.bold,
+        color: AppColors.xf6f6f7,
+      ),
+    ),
   );
   static ThemeData darkTheme = ThemeData(
-    useMaterial3: true,
-    brightness:  Brightness.light,
-    primaryColor: Colors.white,
-    scaffoldBackgroundColor: Colors.black,
-  );
+      useMaterial3: true,
+      brightness: Brightness.light,
+      primaryColor: Colors.white,
+      scaffoldBackgroundColor: Colors.black,
+      textTheme: TextTheme(
+        titleLarge: GoogleFonts.roboto(
+          fontSize: 38,
+          fontWeight: FontWeight.bold,
+          color: Colors.white,
+        ),
+      ));
 }
